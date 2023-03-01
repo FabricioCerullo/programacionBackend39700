53b40ec038f1aafc06de10232dee77edef9431c9
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-03-01 20:42:07 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,10 +5,15 @@ const app = express();
 const manager = new ProductManager("./prod.json");
 let prod = await manager.getProducts();
 
-app.get("/products", async (req, res) => {
-    res.send(prod);
-});
 
+app.get("/products", async (req, res) => {
+    const { limit } = req.query;
+    if (!limit) {
+        res.send(prod);
+    }else{
+        res.send(prod.slice(0, limit));
+    }
+});
 app.get("/products/:id", async (req, res) => {
     let numeroProduct = parseInt(req.params.id);
     let prodFind = prod.find((x)=>x.id===numeroProduct);
@@ -17,11 +22,6 @@ app.get("/products/:id", async (req, res) => {
     }
     res.send(prodFind);
 });
-
-app.get("/products", async (req, res) => {
-    const { limit } = req.query;
-     res.send(prod.slice(0, limit));
- });
    
 app.listen(8080, () => {
     console.log(`Server listening to port 8080`)
