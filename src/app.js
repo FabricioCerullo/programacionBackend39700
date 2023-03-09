eb09d943931e699a67ba17e5976c14a3cebeb180
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-03-09 19:41:42 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,6 +9,6 @@ const app = express();
 app.use("/api/products", productRouter);
 app.use("/api/cart", cartRouter);
 
-app.listen(8081, ()=>{
+app.listen(8080, ()=>{
     console.log("Server listening on port 8080");
 });
